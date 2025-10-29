1f8dd3827cc036fd3fa38f5f048bd52ca05ecb29
Caike Elias Campos Saud <caikeeliass@gmail.com>
2025-10-29 14:00:26 -0300

    Adição de um jogador, funções e alterações
    Agora temos um player, a batalha e as informações da batalha.

--- RPG.py
+++ RPG.py
@@ -2,14 +2,24 @@ from random import randint
 
 lista_npcs = []
 
-def criar_npc():
-    level = randint(0, 50)
+player = {
+    "nome": "SungJinWoo",
+    "level": 1,
+    "exp": 0,
+    "expMAX": 50,
+    "hp": 100,
+    "hp_max": 100,
+    "dano": 25,
+}
+
+def criar_npc(level):
 
     novo_npc = {
         "nome": f"Monstro #{level}",
         "level": level,
         "dano": 5 * level,
         "hp": 100 * level,
+        "exp": 7 * level
     }
 
     return novo_npc
@@ -17,20 +27,37 @@ def criar_npc():
 
 def gerar_npcs(n_npcs):
     for x in range(n_npcs):
-        npc = criar_npc()
+        npc = criar_npc(x + 1)
         lista_npcs.append(npc)
 
 
 def exibir_npcs():
     for npc in lista_npcs:
         print(
-            f"Nome: {npc['nome']} // Level: {npc['level']} // Dano: {npc['dano']} // HP: {npc['hp']}"
+            f"Nome: {npc['nome']} // Level: {npc['level']} // Dano: {npc['dano']} // HP: {npc['hp']} // EXP: {npc['exp']}"
         )
 
 
+def iniciar_batalha(npc):
+    atacar_npc(npc)
+    atacar_player(npc)
+    exibir_info_batalha(npc)
 
+
+def atacar_npc(npc):
+    npc["hp"] -= player ["dano"]
+
+def atacar_player(npc):
+    player["hp"] -= npc["dano"]
+
+def exibir_info_batalha(npc):
+    print(f"Player HP {player['hp']}/{player['hp_max']}")
+    print(f"NPC HP {npc['hp']}/{npc['hp']}")
 
 
 gerar_npcs(5)
-exibir_npcs()
+#exibir_npcs()
 
+
+npc_selecionado = lista_npcs[0]
+iniciar_batalha(npc_selecionado)
